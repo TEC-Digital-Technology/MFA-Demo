65fcedd1661de1bed8c8cd1a3d1d296543a25788
Antony <Antony@tecyt.com>
2024-10-06 14:37:01 +0800

    1. 初始化專案 2. 新增 Swagger 及 API 套件

---
+++ MfaDemo/MfaDemo.WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace MfaDemo.WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API 設定和服務
+
+
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "swagger",
+                routeTemplate: "",
+                defaults: null,
+                constraints: null,
+                handler: new Swashbuckle.Application.RedirectHandler((message => message.RequestUri.ToString()), "swagger"));
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MfaDemo/MfaDemo.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MfaDemo.WebApi.Controllers
+{
+    public class AccountController : ApiController
+    {
+        /// <summary>
+        /// 測試用的
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<string> SomeAction()
+        {
+            return "123";
+        }
+    }
+}
