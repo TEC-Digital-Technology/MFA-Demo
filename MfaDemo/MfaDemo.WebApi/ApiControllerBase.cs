a7b5351cbfe31d7aefdad2ab0ba5c4e40fc42a72
Antony <Antony@tecyt.com>
2024-10-18 11:19:47 +0800

    加入基底型別及回應規範

---
+++ MfaDemo/MfaDemo.WebApi/ApiControllerBase.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+
+namespace MfaDemo.WebApi
+{
+    /// <summary>
+    /// API 控制器基底型別
+    /// </summary>
+    public abstract class ApiControllerBase : ApiController
+    {
+        /// <summary>
+        /// 初始化 API 控制器的基底型別
+        /// </summary>
+        protected ApiControllerBase() { }
+
+        /// <summary>
+        /// 建立回應物件
+        /// </summary>
+        /// <param name="code">回傳代號</param>
+        /// <param name="additionalProperties">附加屬性集合</param>
+        /// <returns></returns>
+        protected HttpResponseMessage GenerateResponse(string code, params (string Key, string Value)[] additionalProperties)
+        {
+            string message;
+            switch (code.ToLower())
+            {
+                case "0000":
+                    message = "成功";
+                    break;
+                case "1001":
+                    message = "帳號已存在";
+                    break;
+                default:
+                    throw new NotImplementedException($"尚未實作代號「{code}」對應的訊息({nameof(message)})。");
+            }
+            Dictionary<string, string> result = new Dictionary<string, string>()
+            {
+                {"ResultCode", code},
+                {"Message", message}
+            };
+            additionalProperties.ToList().ForEach(additionalProperty => result.Add(additionalProperty.Key, additionalProperty.Value));
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(result), System.Text.Encoding.UTF8, "application/json")
+            };
+        }
+        /// <summary>
+        /// 建立處理成功( code = 0000 )的回應物件
+        /// </summary>
+        /// <param name="additionalProperties">附加屬性集合</param>
+        /// <returns></returns>
+        protected HttpResponseMessage GenerateResponse(params (string Key, string Value)[] additionalProperties)
+        {
+            return this.GenerateResponse("0000", additionalProperties);
+        }
+    }
+}
--- MfaDemo/MfaDemo.WebApi/Controllers/AccountController.cs
+++ MfaDemo/MfaDemo.WebApi/Controllers/AccountController.cs
@@ -1,23 +1,61 @@
-﻿using System;
+﻿using MfaDemo.Core.UIData;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace MfaDemo.WebApi.Controllers
 {
-    public class AccountController : ApiController
+    /// <summary>
+    /// 帳號控制器
+    /// </summary>
+    public class AccountController : ApiControllerBase
     {
         /// <summary>
-        /// 測試用的
+        /// 新增使用者
         /// </summary>
+        /// <param name="username">帳號</param>
+        /// <param name="password">密碼</param>
+        /// <param name="email">信箱</param>
         /// <returns></returns>
-        [HttpGet]
-        public async Task<string> SomeAction()
+        [HttpPost]
+        public async Task<HttpResponseMessage> AddUser(string username, string password, string email)
         {
-            return "123";
+            AccountUIData accountUIData = new AccountUIData();
+            if (await accountUIData.GetAccountAsync(username) != null)
+            {
+                return base.GenerateResponse("1001");//帳號存在
+            }
+            var accountInfo = await accountUIData.AddAccountAsync(username, password, email);
+            return base.GenerateResponse(("AccountId", accountInfo.Id.ToString()));
+        }
+
+        /// <summary>
+        /// 驗證使用者帳號及密碼
+        /// </summary>
+        /// <param name="username">帳號</param>
+        /// <param name="password">密碼</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<HttpResponseMessage> AuthenticateUser(string username, string password)
+        {
+            return base.GenerateResponse();
+        }
+
+        /// <summary>
+        /// 發送信件 OTP 驗證碼
+        /// </summary>
+        /// <param name="accountId">帳號 ID</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<HttpResponseMessage> SendSmtpOtp(Guid accountId)
+        {
+            return base.GenerateResponse();
         }
     }
 }
